747c13bd0fcaaf55b06a6696b7d7fe5e75368432
ss <mkt.lissettemartinez@gmail.com>
2026-09-21 00:36:13 -0600

    Renovar caché offline tras correcciones

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const C='atl-btl-ttl-v4';const A=['./','./index.html','./manifest.webmanifest','./payload0.txt','./payload1.txt','./payload2.txt','./payload3.txt'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html'))))});
+const C='atl-btl-ttl-v8';
+const A=['./','./index.html','./manifest.webmanifest','./payload0.txt','./payload1.txt','./payload2.txt','./payload3.txt','./payload4.txt'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ e.respondWith(fetch(e.request).then(resp=>{
+   const copy=resp.clone();
+   caches.open(C).then(c=>c.put(e.request,copy));
+   return resp;
+ }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
